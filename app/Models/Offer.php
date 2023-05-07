c635cbc034702a83282f2d8b02f65b535c8b8fae
mohammadodeh96 <mohbus996@gmail.com>
2023-05-07 17:49:40 +0300

    ..

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -10,7 +10,7 @@ class Offer extends Model
     use HasFactory;
 
 
-    protected $fillable = ['description', 'store_id','daysToEnd'];
+    protected $fillable = ['description', 'store_id','daysToEnd','images'];
 
     public function store()
     {
